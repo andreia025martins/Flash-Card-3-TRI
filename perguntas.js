7e5a92e7fb2fd6d82724157a40bdfb96151aa144
Andréia <andreia.martins17@escola.pr.gov.br>
2024-11-04 19:20:40 -0300

    ajuste

--- perguntas.js
+++ perguntas.js
@@ -13,3 +13,18 @@ criarCartao(
     'O que é uma função?',
     'Uma função é um bloco de código que executa alguma tarefa'
 )
+criarCartao(
+    'Programação',
+    'O que é WEB?',
+    'A World Wide Web designa um sistema de documentos em hipermídia que são interligados e executados na Internet. Os documentos podem estar na forma de vídeos, sons, hipertextos e imagens'
+)
+criarCartao(
+    'Programação',
+    'O que é Racionalismo?',
+    'O Racionalismo é uma corrente filosófica que atribui particular confiança à razão humana, ao passo que acredita que é dela que se obtém os conhecimentos.'
+)
+criarCartao(
+    'Programação',
+    'O que é WEB?',
+    'A World Wide Web designa um sistema de documentos em hipermídia que são interligados e executados na Internet. Os documentos podem estar na forma de vídeos, sons, hipertextos e imagens'
+)
